3663705309042b7c73a3beacfd68a739f26d3d31
zakio826 <fki2266287@stu.o-hara.ac.jp>
2024-02-01 11:22:56 +0900

    ログインフォームのアクションの指定を修正

--- src/app/accounts/login.php
+++ src/app/accounts/login.php
@@ -109,7 +109,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }        
         ?>
 
-        <form action="<?php echo $_SERVER ['SCRIPT_NAME']; ?>" method="post">
+        <form action="<?php echo $path; ?>" method="post">
             <div class="form-group">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty(h($errors['username']))) ? 'is-invalid' : ''; ?>" value="<?php echo h($datas['username']); ?>">
