b3ebf3a598e50dda37ed5402c793be38276dd300
yamamoto0727 <fki2266275@stu.o-hara.ac.jp>
2024-02-02 12:03:58 +0900

    複数の目標を表示できるようにした

--- src/app/goal/level_of_achievement.php
+++ src/app/goal/level_of_achievement.php
@@ -9,34 +9,46 @@ $level_of_achievement_class = new level_of_achievement_class($db);
 $have_points = $level_of_achievement_class->getHave_points();
 $savings = $level_of_achievement_class->getSavings();
 $have_money = $have_points+$savings;
-$target_amount = $level_of_achievement_class->getTarget_amount();
-$requiredpoint = $level_of_achievement_class->getRequired_point();
-$onerequiredpoint = $level_of_achievement_class->getOnerequired_point();
-unset($_SESSION['join']);
 ?>
 
-<?php
-$page_title = "目標達成度";
-require_once("../include/header.php");
-?>
-
-<main>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>目標達成度</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ 
+            font: 14px sans-serif;
+            text-align: center; 
+        }
+    </style>
+</head>
+<body>
     <div class="container">
     <h1>達成度の状況</h1>
-        <p><strong>現在のお金（ポイント＋貯金）:</strong> <?php echo htmlspecialchars($have_money); ?> 円</p>
-        <p>内訳</h3>
-        <p>ポイント:</strong> <?php echo htmlspecialchars($have_points); ?> ポイント</p>
-        <p>貯金:</strong> <?php echo htmlspecialchars($savings); ?> 円</p>
-
-        <p>目標金額:</strong> <?php echo htmlspecialchars($target_amount); ?> 円</p>
-        <p>必要ポイント:</strong> <?php echo htmlspecialchars($requiredpoint); ?> 円</p>
-        <p>1日の必要ポイント:</strong> <?php echo htmlspecialchars($onerequiredpoint); ?> 円</p>
+    <p><strong>現在のお金（ポイント＋貯金）:</strong> <?php echo htmlspecialchars($have_money); ?> 円</p>
+    <p>内訳</h3>
+    <p>ポイント:</strong> <?php echo htmlspecialchars($have_points); ?> ポイント</p>
+    <p>貯金:</strong> <?php echo htmlspecialchars($savings); ?> 円</p>
+    <hr>
+    <?php if(count($level_of_achievement_class->getGoal()) != 0){ ?>
+        <?php for($i=0;$i<count($level_of_achievement_class->getGoal());$i++){ ?>
+            <p>いつまで:<?php echo htmlspecialchars($level_of_achievement_class->getGoal_deadline($i)); ?> </p>
+            <p>内容:<?php echo htmlspecialchars($level_of_achievement_class->getGoal_detail($i)); ?> </p>
+            <p>目標金額:<?php echo htmlspecialchars($level_of_achievement_class->getTarget_amount($i)); ?> 円</p>
+            <p>必要ポイント:<?php echo htmlspecialchars($level_of_achievement_class->getRequired_point($i)); ?> ポイント</p>
+            <p>1日の必要ポイント:<?php echo htmlspecialchars($level_of_achievement_class->getOnerequired_point($i)); ?> ポイント</p>
+            <hr>
+        <?php } ?>
+    <?php } else { ?>
+        <p>目標を設定してください</p>
+    <?php } ?>
 
 
         <p class="mt-3">
-            <a href="../index.php" class="btn btn-primary">戻る</a>
+            <a href="../accounts/welcome.php" class="btn btn-primary">ホーム</a>
         </p>
     </div>
-</main>
-
-<?php require_once("../include/footer.php"); ?>
+</body>
+</html>
