b778e98e9874d003cabc793367e4a3bdbeba8e78
zakio826 <fki2266287@stu.o-hara.ac.jp>
2024-02-02 16:23:09 +0900

    カレンダー

--- src/app/record/calendar.php
+++ src/app/record/calendar.php
@@ -3,16 +3,60 @@
 <!-- ヘッダー -->
 <?php
 $page_title = "カレンダー";
-require_once("../include/header.php");
+include("../include/header.php");
 ?>
 
-<main>
-    <!-- ナビゲーションバー -->
-    <?php include("../include/nav_bar.php") ?>
+<style>
+    thead, tbody, tr, th, td {
+        border-collapse: collapse;
+        border: 0.5px solid #333;
+        /* border-color: black; */
+        /* border-style: solid; */
+        border-width: unset;
+    }
+</style>
 
-    <section class="position-relative h-75">
+<!-- ナビゲーションバー -->
+<?php include_once("../include/nav_bar.php") ?>
+
+<main>
+    <section class="position-relative">
+        <div class="container">
+            <div class="row mt-3">
+                <div class="position-relative px-5">
+                    <table class="w-100" style="caption-side: top;">
+                        <!-- 月の変更 -->
+                        <caption class="text-center">
+                            <h4 class="my-0">
+                                <span class="me-1">2024</span>年
+                            </h4>
+                            <h1 class="pt-0" style="color: black;">
+                                <span>＜</span>
+                                <span>1</span>月
+                                <span>＞</span>
+                            </h1>
+                        </caption>
+                        
+                        <!-- カレンダー -->
+                        <thead class="text-center" style="background-color: white;">
+                            <tr class="text-center">
+                                <th><font color="red">日</font></th>
+                                <th>月</th><th>火</th><th>水</th><th>木</th><th>金</th>
+                                <th><font color="blue">土</font></th>
+                            </tr>
+                        </thead>
+                        <tbody class="w-100" style="background-color: white;">
+                        </tbody>
+                    </table>
+                </div>
+                <!-- <div class="position-relative px-4">
+                </div> -->
+            </div>
+
+            <div class="row"></div>
+        </div>
     </section>
 </main>
 
 <!-- フッター -->
-<?php require_once("../include/footer.php"); ?>
+<?php include_once("../include/footer.php"); ?>
