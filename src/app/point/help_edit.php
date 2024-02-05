9ac17c93eccc74adfe6ec4d07158aed342443734
kit829 <fki2266255@stu.o-hara.ac.jp>
2024-02-05 17:00:45 +0900

    v

--- src/app/point/help_edit.php
+++ src/app/point/help_edit.php
@@ -1,22 +1,35 @@
+
+<?php
+require("../../../lib/help_class.php");
+
+?>
+
 <!DOCTYPE html>
-<html lang="jp">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>編集画面</title>
-</head>
+    <meta charset="utf-8">
+    <title>編集</title>
 
-<body>
-    <div class="title-area">
-        <h1>編集画面</h1>
+    <div class="contact-form">
+        <h2>編集</h2>
+        <form action="help_edit_process.php" method="post">
+            <p>
+                <label>お手伝い名：</label>
+                <input type="text" name="help_name" value="<?php if (!empty($result['help_name'])) echo(htmlspecialchars($result['help_name'], ENT_QUOTES, 'UTF-8'));?>">
+            </p>
+            <p>
+                <label>お手伝い詳細：</label>
+                <input type="text" name="help_detail" value="<?php if (!empty($result['help_detail'])) echo(htmlspecialchars($result['help_detail'], ENT_QUOTES, 'UTF-8'));?>">
+            </p>
+            <p>
+                <label>獲得ポイント：</label>
+                <input type="number" name="get_point" value="<?php if (!empty($result['get_point'])) echo(htmlspecialchars($result['get_point'], ENT_QUOTES, 'UTF-8'));?>">
+            </p>
+
+            <input type="submit" value="編集する">
+
+        </form>
     </div>
-    <form action="" method="POST">
-        <input type="text" class="input-area" name="title" placeholder="Title"><br>
-        <input type="text" class="input-area" name="content" placeholder="Content"><br>
-        <input type="submit" class="input-area submit" name="submit" value="更新">
-    </form>
+        <a href="help_add.php">投稿一覧へ</a>
 </body>
-
 </html>
