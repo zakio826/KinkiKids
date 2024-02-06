82d5e3cc304784cdbac9e3a3a890d8366e517ad3
yamamoto0727 <fki2266275@stu.o-hara.ac.jp>
2024-02-06 12:59:52 +0900

    ちょっと変えた

--- src/app/index_child.php
+++ src/app/index_child.php
@@ -22,12 +22,99 @@ $help_count = $index_child_class->getHelpCount();
 $message_count = $index_child_class->getMessageCount();
 ?>
 
+
+<main>
+    <!-- ナビゲーションバー -->
+    <?php include_once("./include/nav_bar.php") ?>
+
+    <!-- ロゴ -->
+    <header class="position-relative h-25" style="padding-top: 4rem;">
+        <img class="d-block mx-auto py-3" src="<?php echo $absolute_path; ?>static/assets/logo.png" height="120">
+    </header>
+    
+    <section class="position-relative h-75">
+        <?php if($goal_count != 0){ ?>
+            <a href="./goal/goal_detail.php">
+                もくひょう<br>
+                <?php echo htmlspecialchars($index_child_class->getGoal_detail()); ?><br>
+                <?php echo htmlspecialchars($index_child_class->getGoal_deadline()); ?> 
+                <?php echo htmlspecialchars($index_child_class->getTarget_amount()); ?> 円
+            </a>
+        <?php } else { ?>
+            <p>目標がないので設定してください</p>
+        <?php } ?>
+        <hr>
+        ちょきん: <?php echo htmlspecialchars($savings); ?> えん　　
+        てもち: <?php echo htmlspecialchars($have_points); ?> ポイント
+        <p>ごうけい: <?php echo htmlspecialchars($have_money); ?> えん</p>
+        <p>きょうかせぐポイント: <?php echo htmlspecialchars($index_child_class->getOnerequired_point()); ?> ポイント</p>
+        <hr>
+
+        <div class="modal-2__wrap"> 
+            <input type="radio" id="modal-2__open" class="modal-2__open-input" name="modal-2__trigger"/>
+            <label for="modal-2__open" class="modal-2__open-label">きょうのおてつだいをひょうじ</label>
+            <input type="radio" id="modal-2__close" name="modal-2__trigger"/>
+            <div class="modal-2">
+                <div class="modal-2__content-wrap">
+                    <label for="modal-2__close" class="modal-2__close-label">×</label>
+                    <div class="modal-2__content">
+                    <p>みっしょん</p>
+                    <?php if($help_count != 0){ ?>
+                        <?php for($i=0;$i<$help_count;$i++){ ?>
+                            <p>・<?php echo htmlspecialchars($index_child_class->getHelp($i)); ?> </p>
+                            <hr>
+                        <?php } ?>
+                    <?php } else { ?>
+                            <p>お手伝いを設定してください</p>
+                    <?php } ?>
+
+                    </div>
+                </div>
+                <label for="modal-2__close">
+                    <div class="modal-2__background"></div>
+                </label>
+            </div>
+        </div>
+        <hr>
+        <p>メッセージ</p>
+        <select id="user_select">
+            <option value=""></option>
+            <?php $index_child_class->getFamilyUser(); ?>
+        </select>
+        <p id="order-string"></p>
+        <br>
+
+        <?php if($message_count != 0){ ?>
+            <?php for($i=0;$i<$message_count;$i++){ ?>
+                <?php echo htmlspecialchars($index_child_class->getMessage($i)['sender']); ?>
+                ➡
+                <?php echo htmlspecialchars($index_child_class->getMessage($i)['receiver']); ?>
+                <p><?php echo htmlspecialchars($index_child_class->getMessage($i)['messagetext']); ?> </p>
+                <hr>
+            <?php } ?>
+        <?php } else { ?>
+            <p>メッセージがありません</p>
+        <?php } ?>
+    </section>
+</main>
+<!-- フッター -->
+<?php include_once("./include/footer.php"); ?>
+
+<script>
+    const select = document.getElementById('user_select');
+    select.addEventListener('change', (e) => {
+        let selected_value = document.getElementById('user_select').value;
+        document.getElementById('order-string').innerHTML = selected_value;
+    });
+    // return selected_value
+</script>
+        
 <style>
     .action-btn {
-        background-color: lemonchiffon;
-        border-radius: 2rem;
-        box-shadow: 0 6px 8px 0 rgba(0, 0, 0, .5);
-        /* height: 30%; */
+    background-color: lemonchiffon;
+    border-radius: 2rem;
+    box-shadow: 0 6px 8px 0 rgba(0, 0, 0, .5);
+    /* height: 30%; */
     }
     .modal-2__wrap input {
     display: none;
@@ -136,74 +223,3 @@ $message_count = $index_child_class->getMessageCount();
         }
     }
 </style>
-
-<main>
-    <!-- ナビゲーションバー -->
-    <?php include_once("./include/nav_bar.php") ?>
-
-    <!-- ロゴ -->
-    <header class="position-relative h-25" style="padding-top: 4rem;">
-        <img class="d-block mx-auto py-3" src="<?php echo $absolute_path; ?>static/assets/logo.png" height="120">
-    </header>
-    
-    <section class="position-relative h-75">
-        <?php if($goal_count != 0){ ?>
-            <a href="./goal/goal_detail.php">
-                もくひょう<br>
-                <?php echo htmlspecialchars($index_child_class->getGoal_detail()); ?><br>
-                <?php echo htmlspecialchars($index_child_class->getGoal_deadline()); ?> 
-                <?php echo htmlspecialchars($index_child_class->getTarget_amount()); ?> 円
-            </a>
-        <?php } else { ?>
-            <p>目標がないので設定してください</p>
-        <?php } ?>
-        <hr>
-        ちょきん: <?php echo htmlspecialchars($savings); ?> えん　　
-        てもち: <?php echo htmlspecialchars($have_points); ?> ポイント
-        <p>ごうけい: <?php echo htmlspecialchars($have_money); ?> えん</p>
-        <p>きょうかせぐポイント: <?php echo htmlspecialchars($index_child_class->getOnerequired_point()); ?> ポイント</p>
-        <hr>
-
-        <div class="modal-2__wrap"> 
-            <input type="radio" id="modal-2__open" class="modal-2__open-input" name="modal-2__trigger"/>
-            <label for="modal-2__open" class="modal-2__open-label">きょうのおてつだいをひょうじ</label>
-            <input type="radio" id="modal-2__close" name="modal-2__trigger"/>
-            <div class="modal-2">
-                <div class="modal-2__content-wrap">
-                    <label for="modal-2__close" class="modal-2__close-label">×</label>
-                    <div class="modal-2__content">
-                    <p>みっしょん</p>
-                    <?php if($help_count != 0){ ?>
-                        <?php for($i=0;$i<$help_count;$i++){ ?>
-                            <p>・<?php echo htmlspecialchars($index_child_class->getHelp($i)); ?> </p>
-                            <hr>
-                        <?php } ?>
-                    <?php } else { ?>
-                            <p>お手伝いを設定してください</p>
-                    <?php } ?>
-
-                    </div>
-                </div>
-                <label for="modal-2__close">
-                    <div class="modal-2__background"></div>
-                </label>
-            </div>
-        </div>
-        <hr>
-        <p>メッセージ</p>
-        <?php if($message_count != 0){ ?>
-            <?php for($i=0;$i<$message_count;$i++){ ?>
-                <?php echo htmlspecialchars($index_child_class->getMessage($i)['sender']); ?>
-                ➡
-                <?php echo htmlspecialchars($index_child_class->getMessage($i)['receiver']); ?>
-                <p><?php echo htmlspecialchars($index_child_class->getMessage($i)['messagetext']); ?> </p>
-                <hr>
-            <?php } ?>
-        <?php } else { ?>
-                <p>メッセージがありません</p>
-        <?php } ?>
-    </section>
-</main>
-
-<!-- フッター -->
-<?php include_once("./include/footer.php"); ?>
