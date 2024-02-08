56525ba7318c1252fd29bd77125a2dfa9e5b38cd
kimuti <fki2266264@stu.o-hara.ac.jp>
2024-02-08 11:08:38 +0900

    編集削除ボタン変更

--- src/app/point/help_add.php
+++ src/app/point/help_add.php
@@ -58,38 +58,37 @@ $helps = $help->display_help($family_id);
         <?php if (empty($helps)): ?>
             <p>お手伝いはありません。</p>
         <?php else: ?>
-            <ul>
-                <?php foreach ($helps as $help_data): ?>
-                        <strong>お手伝い名:</strong> <?php echo $help_data['help_name']; ?><br>
-                        <strong>お手伝い詳細</strong> <?php echo $help_data['help_detail']; ?><br>
-                        <strong>獲得ポイント:</strong> <?php echo $help_data['get_point']; ?><br>
-                        <strong>担当者</strong>
-                        <?php
-                            $help->person_select($help_data['help_id']);
-                        ?><br>
-                    <hr>
+            <?php foreach ($helps as $help_data): ?>
+                    <strong>お手伝い名:</strong> <?php echo $help_data['help_name']; ?><br>
+                    <strong>お手伝い詳細</strong> <?php echo $help_data['help_detail']; ?><br>
+                    <strong>獲得ポイント:</strong> <?php echo $help_data['get_point']; ?><br>
+                    <strong>担当者</strong>
+                    <?php
+                        $help->person_select($help_data['help_id']);
+                    ?><br>
+                <div class="btn-group">
                     <?php if ($select === 'adult'): ?>
                         <form action="help_edit.php" method="get">
                             <input type="hidden" name="edit_help_id" value="<?php echo $help_data['help_id']; ?>">
-                            <button type="submit">編集</button>
+                            <button type="submit" class="btn-1">編集</button>
                         </form>
                         
                         <form action="" method="post">
                             <input type="hidden" name="delete_help_id" value="<?php echo $help_data['help_id']; ?>">
-                            <button type="submit">削除</button>
+                            <button type="submit" class="btn-2">削除</button>
                         </form>
                     <?php endif; ?>
-
-                    <?php if ($select === 'child'): ?>
-                        <form action="" method="post">
-                            <input type="hidden" name="consent_help_id" value="<?php echo $help_data['help_id']; ?>">
-                            <?php
-                            $help->consent_button($help_data['help_id']);
-                            ?>
-                        </form>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </ul>
+                </div>
+                <hr>
+                <?php if ($select === 'child'): ?>
+                    <form action="" method="post">
+                        <input type="hidden" name="consent_help_id" value="<?php echo $help_data['help_id']; ?>">
+                        <?php
+                        $help->consent_button($help_data['help_id']);
+                        ?>
+                    </form>
+                <?php endif; ?>
+            <?php endforeach; ?>
         <?php endif; ?>
 
         <p class="mt-3"><a href="../welcome.php" class="btn btn-primary">もどる</a></p>
