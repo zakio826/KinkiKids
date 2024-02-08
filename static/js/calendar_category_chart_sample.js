a6b2f80f413a63f7e81f86e2298aed42b4191bc7
zakio826 <fki2266287@stu.o-hara.ac.jp>
2024-02-08 17:02:11 +0900

    データベースから表示月以前5か月分の月間収支を取得してグラフに表示する

---
+++ static/js/calendar_category_chart_sample.js
@@ -0,0 +1,49 @@
+/* 
+ * line         線グラフ
+ * bar          棒グラフ
+ * radar        レーダーチャート
+ * pie          円グラフ
+ * doughnut     ドーナツチャート
+ * polarArea    鶏頭図(値によって半径が異なる円グラフ)
+ * bubble       バブルチャート
+ * scatter      散布図
+ */
+
+
+let categoryChartData = {
+    labels: ['赤', '青', '黄'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }
+    ]
+};
+
+let categoryChartOption = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+};
+
+
+const categoryCtx = document.getElementById('categoryChart').getContext('2d');
+
+let categoryChart = new Chart(categoryCtx, {
+    type: 'pie',  // 円グラフ
+    data: categoryChartData,
+    options: categoryChartOption
+});
