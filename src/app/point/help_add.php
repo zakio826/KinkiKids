d713180078ebf9244f1b159426381a4db7693017
takegoshi54 <fki2266215@gmail.com>
2024-02-09 20:51:13 +0900

    tk2.9

--- src/app/point/help_add.php
+++ src/app/point/help_add.php
@@ -99,13 +99,13 @@ if (isset($_POST["narrow"]) && !empty($_POST["narrow"])) {
                         <button type="submit" class="btn-2">削除</button>
                     </form>
                 <?php endif; ?>
-                <hr>
                 <?php if ($select === 'child'): ?>
                     <form action="" method="post">
                         <input type="hidden" name="consent_help_id" value="<?php echo $help_data['help_id']; ?>">
                         <?php $help->consent_button($help_data['help_id']); ?>
                     </form>
                 <?php endif; ?>
+                <hr>
                 </ul>
             <?php endforeach; ?>
         <?php endif; ?>
