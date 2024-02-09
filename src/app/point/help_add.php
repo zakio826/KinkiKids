6dd67a387d26dc2ad4f965eb221a67d8a0097f08
takegoshi54 <fki2266215@gmail.com>
2024-02-09 20:50:11 +0900

    tk2.9

--- src/app/point/help_add.php
+++ src/app/point/help_add.php
@@ -101,14 +101,6 @@ if (isset($_POST["narrow"]) && !empty($_POST["narrow"])) {
                 <?php endif; ?>
                 <hr>
                 <?php if ($select === 'child'): ?>
-                    <li>
-                        <strong>お手伝い名:</strong> <?php echo $help_data['help_name']; ?><br>
-                        <strong>獲得ポイント:</strong> <?php echo $help_data['get_point']; ?><br>
-                        <strong>担当者</strong>
-                        <?php
-                            $help->person_select($help_data['help_id']);
-                        ?><br>
-                    </li>
                     <form action="" method="post">
                         <input type="hidden" name="consent_help_id" value="<?php echo $help_data['help_id']; ?>">
                         <?php $help->consent_button($help_data['help_id']); ?>
