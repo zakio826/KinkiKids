bb3a409950ba5efaf309eb09bf9c624b232b558b
Taka92432 <tetuokitagawa9@gmail.com>
2024-02-09 10:02:00 +0900

    項目の削除

--- src/app/spending/spending_input.php
+++ src/app/spending/spending_input.php
@@ -65,45 +65,11 @@ include("../include/header.php");
             <?php endwhile; ?>
         </select>
         <!-- <a class="c-button c-button--bg-gray" href="./item-edit.php">編集</a> -->
-        </div>
+		<div>
+			<textarea name="memo" id="" cols="45" rows="5" placeholder="入力収支の詳細"></textarea>
+		</div>
 
-			<div class="u-js__show-switch flex p-form__flex-input sp-change-order" id="spendingCategoryBox">
-				<p class="long-name">支出カテゴリー</p>
-				<select name="spending_category" id="spendingCategory">
-					<option value="0">選択してください</option>
-					<option value="1">食費</option>
-					<option value="2">日用品</option>
-					<option value="3">交通費</option>
-				</select>
-				<!-- <a class="c-button c-button--bg-gray" href="./item-edit.php">編集</a> -->
-			</div>
-
-			<div class="u-js__show-switch flex p-form__flex-input sp-change-order" id="incomeCategoryBox">
-				<p class="long-name">収入カテゴリー</p>
-				<select name="income_category" id="incomeCategory">
-					<option value="0">選択してください</option>
-					<option value="1">給料</option>
-					<option value="2">お小遣い</option>
-				</select>
-				<!-- <a class="c-button c-button--bg-gray" href="./item-edit.php">編集</a> -->
-			</div>
-
-			<div id="paymentMethodBox" class="u-js__show-switch flex p-form__flex-input sp-change-order">
-				<p class="long-name">支払い方法</p>
-				<select name="payment_method" id="paymentMethod" onchange="hasChildSelect('2', creditSelectBox, qrChecked);hasChildSelect('3', qrSelectBox, creditChecked);">
-					<option value="0">選択してください</option>
-					<option value="1">現金</option>
-					<option value="2" id="radioCredit">クレジット</option>
-					<option value="3" id="radioQr">スマホ決済</option>
-				</select>
-				<!-- <a class="c-button c-button--bg-gray" href="./item-edit.php">編集</a> -->
-			</div>
-
-			<div>
-				<textarea name="memo" id="" cols="45" rows="5" placeholder="入力収支の詳細"></textarea>
-			</div>
-
-			<input class="c-button c-button--bg-blue" type="submit" value="登録">
+		<input class="c-button c-button--bg-blue" type="submit" value="登録">
 		</form>
 	</section>
 </main>
