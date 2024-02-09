8e59ea7ce516735425fed987c7f6e431128628f8
kimuti <fki2266264@stu.o-hara.ac.jp>
2024-02-09 11:01:09 +0900

    setting_normaのcss変更

--- src/app/point_norma/setting_norma.php
+++ src/app/point_norma/setting_norma.php
@@ -22,20 +22,22 @@ include("../include/header.php");
             <form action="" method="POST">
                 <h1>ポイントノルマ設定</h1>
 
-                <div class="mt-3 control-1">
+                <div class="control-1">
                     <label for="norma_amount">ポイントノルマ</label>
                     <input id="norma_amount" type="number" name="norma_amount" value="<?php echo isset($_SESSION['join']['norma_amount']) ? htmlspecialchars($_SESSION['join']['norma_amount'], ENT_QUOTES) : ''; ?>">
-                    <b>pt</b>
+                    <!-- <b>pt</b> -->
                     <?php $setting_norma->norma_error(); ?>
                 </div>
 
-                <div class="mb-3 control-1">
-                    <label for="point_norma_deadline">期限</label>
+                <div class="control-1">
+                    <label for="point_norma_deadline">いつまで？</label>
                     <input id="point_norma_deadline" type="date" name="point_norma_deadline" value="<?php echo isset($_SESSION['join']['point_norma_deadline']) ? htmlspecialchars($_SESSION['join']['point_norma_deadline'], ENT_QUOTES) : ''; ?>">
                     <?php $setting_norma->deadline_error(); ?>
                 </div>
 
-                <div class="control-2"><button type="submit" class="btn">登録</button></div>
+                <div class="control-2">
+                    <button type="submit" class="btn">登録</button>
+                </div>
             </form>
         </div>
     </section>
