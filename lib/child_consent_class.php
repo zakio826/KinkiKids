71970b9fe1d67e0e07c9c8d5b422b94a9d6188b1
takegoshi54 <fki2266215@gmail.com>
2024-02-11 03:05:40 +0900

    tk2.11

--- lib/child_consent_class.php
+++ lib/child_consent_class.php
@@ -25,6 +25,22 @@
                 $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
                 echo $get_point."ポイント獲得しました";
+            }elseif(isset($_POST["child_consent_mission_log_id"]) && isset($_POST["child_consent_user_id"]) && isset($_POST["child_consent_get_point"])){
+                $mission_log_id = $_POST["child_consent_mission_log_id"];
+                $user_id = $_POST["child_consent_user_id"];
+                $get_point = $_POST["child_consent_get_point"];
+                $stmt = $this->db->prepare("UPDATE mission_log SET receive_flag = 0 WHERE mission_log_id = :mission_log_id");
+                $stmt->bindParam(':mission_log_id', $mission_log_id);
+                $stmt->execute();
+                $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                $stmt2 = $this->db->prepare("UPDATE child_data SET have_points = have_points + :get_point WHERE user_id = :user_id");
+                $stmt2->bindParam(':get_point', $get_point);
+                $stmt2->bindParam(':user_id', $user_id);
+                $stmt2->execute();
+                $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+                echo $get_point."ポイント獲得しました";
             }
         }
         public function getHelps($user_id){
@@ -51,6 +67,31 @@
             }
             echo "</li>";
         }
-        public function point_get(){
+
+        public function getmissions($user_id){
+            //TODO ない時の処理？？？
+            $stmt = $this->db->prepare("SELECT mission_id,mission_log_id FROM mission_log WHERE user_id = :user_id and receive_flag = 1");
+            $stmt->bindParam(':user_id', $user_id);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "<li>";
+            foreach($result as $row){
+                $stmt2 = $this->db->prepare("SELECT * FROM mission WHERE mission_id = :mission_id");
+                $stmt2->bindParam(':mission_id', $row['mission_id']);
+                $stmt2->execute();
+                $result2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+                echo "★緊急ミッション★<br>";
+                echo "<strong>ミッション名:</strong>".$result2[0]["mission_name"]."<br>";
+                echo "<strong>獲得ポイント:</strong>".$result2[0]["get_point"]."<br>";
+                echo '
+                <form action="" method="post">       
+                        <input type="hidden" name="child_consent_mission_log_id" value="'.$row["mission_log_id"].'"> 
+                        <input type="hidden" name="child_consent_user_id" value="'.$user_id.'">    
+                        <input type="hidden" name="child_consent_get_point" value="'.$result2[0]["get_point"].'">  
+                        <button type="submit">ポイント獲得</button>
+                </form>
+                ';
+            }
+            echo "</li>";
         }
     }
--- lib/conset_class.php
+++ lib/conset_class.php
@@ -9,7 +9,7 @@ class consent {
         $this->db = $db;
         $this->error = []; // 初期化
 
-            if (isset($_POST["consent_help_id"])){//ポイント追加処理
+            if (isset($_POST["consent_help_id"])){
                 $help_id = $_POST["consent_help_id"];
 
                 $stmt = $this->db->prepare("UPDATE help_log SET receive_flag = 1,consent_flag = 0 WHERE help_id = :help_id and consent_flag = 1");
@@ -18,20 +18,22 @@ class consent {
                 $stmt->fetchAll(PDO::FETCH_ASSOC);
 
                 echo "承認しました";
+                
+            }elseif (isset($_POST["consent_mission_id"])){
+                $mission_id = $_POST["consent_mission_id"];
 
-                // foreach ($result as $row) {
-                //     $stmt2 = $this->db->prepare("UPDATE child_data SET have_points = have_points + :get_point WHERE user_id = :user_id");
-                //     $stmt2->bindParam(':get_point', $get_point);
-                //     $stmt2->bindParam(':user_id', $row['user_id']);
-                //     $stmt2->execute();
-                //     $stmt2->fetchAll(PDO::FETCH_ASSOC);
-                // }
+                $stmt = $this->db->prepare("UPDATE mission_log SET receive_flag = 1,consent_flag = 0 WHERE mission_id = :mission_id and consent_flag = 1");
+                $stmt->bindParam(':mission_id', $mission_id);
+                $stmt->execute();
+                $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                // $stmt = $this->db->prepare("UPDATE help_log SET consent_flag = 0 WHERE consent_flag = 1 and help_id = :help_id");
-                // $stmt->bindParam(':help_id', $help_id);
-                // $stmt->execute();
-                // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                //拒否の場合はdisplayflag戻して
+                $stmt2 = $this->db->prepare("UPDATE mission SET display_flag = 0 WHERE mission_id = :mission_id");
+                $stmt2->bindParam(':mission_id', $mission_id);
+                $stmt2->execute();
+                $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
+                echo "承認しました";
                 
             }
         }
@@ -45,6 +47,16 @@ class consent {
             return $result;
         }
 
+        public function display_consent_mission($user_id) {
+            $stmt = $this->db->prepare("SELECT mission.mission_name,mission.get_point,mission.mission_id FROM mission
+                                        INNER JOIN mission_log ON mission.mission_id = mission_log.mission_id 
+                                        WHERE mission_log.consent_flag = 1 and mission.user_id = :user_id and mission.display_flag = 1");
+            $stmt->bindParam(':user_id', $user_id);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
     public function person_select($help_id) {
         $stmt = $this->db->prepare(
             "SELECT user.first_name FROM user ".
@@ -52,6 +64,24 @@ class consent {
             "WHERE help_id = :help_id"
         );
         $stmt->bindParam(':help_id', $help_id);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $first_flag = 0;
+        foreach ($result as $row) {
+            if ($first_flag != 0) { echo ","; }
+            $first_flag = 1;
+            echo $row['first_name'];
+        }
+    }
+
+    public function m_person_select($mission_id) {
+        $stmt = $this->db->prepare(
+            "SELECT user.first_name FROM user ".
+            "INNER JOIN mission_person ON user.user_id = mission_person.user_id ".
+            "WHERE mission_id = :mission_id"
+        );
+        $stmt->bindParam(':mission_id', $mission_id);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
