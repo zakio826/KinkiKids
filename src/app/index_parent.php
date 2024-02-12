ab15c725e64dabd30efbd53506c28bf1753a2a34
hatanaka <fki2266353@stu.o-hara.ac.jp>
2024-02-12 11:59:32 +0900

    リンクの位置修正

--- src/app/index_parent.php
+++ src/app/index_parent.php
@@ -121,10 +121,7 @@ echo '</script>';
                 <label for="slide2" class="slides-nav"></label>
                 <label for="slide3" class="slides-nav"></label>
             </div>
-
-
-        
-
+            <a href="<?php echo $absolute_path; ?>src/app/point/mission_add.php"><img src="">緊急ミッション</a><a href="<?php echo $absolute_path; ?>src/app/point/consent.php"><img src="">承認待ち</a>
 
         <hr class="index_parent_hr">
 
@@ -132,7 +129,6 @@ echo '</script>';
         <div class="index_parent_messagecss1">
             <div class="index_parent_messagecss2">
                 <div class="index_parent_messagecss3">
-        <a href="<?php echo $absolute_path; ?>src/app/point/mission_add.php"><img src="">緊急ミッション</a><a href="<?php echo $absolute_path; ?>src/app/point/consent.php"><img src="">承認待ち</a>
 
         <!-- <hr class="index_child_hr"> -->
         <div class="index_child_messagecss1">
