518feae508db87c1466e9e436d0b591f6576c0fd
kimuti <fki2266264@stu.o-hara.ac.jp>
2024-02-14 15:14:03 +0900

    クラス名追加

--- src/app/index_parent.php
+++ src/app/index_parent.php
@@ -118,7 +118,7 @@ echo '</script>';
                     <b class="index_parent_mokuhyoumoji">
                         ポイントノルマ：<p id="norma"></p>
                         期限：<p id="norma_deadline"></p>
-                        <a href="<?php echo $absolute_path; ?>src/app/point_norma/setting_norma.php">
+                        <a href="<?php echo $absolute_path; ?>src/app/point_norma/setting_norma.php" class="btn-p">
                             ＋
                         </a>
 
@@ -133,7 +133,7 @@ echo '</script>';
                         報酬ポイント：<p id="reward_point"></p>
                         期限：<p id="behavioral_goal_deadline"></p>
                         <!-- 行動目標に飛ぶボタン -->
-                        <a href="<?php echo $absolute_path; ?>src/app/behavioral_goal/setting_behavioral.php">
+                        <a href="<?php echo $absolute_path; ?>src/app/behavioral_goal/setting_behavioral.php" class="btn-p">
                             ＋
                         </a>
 
@@ -146,7 +146,7 @@ echo '</script>';
         <div class="slideshow">
 
             <form action="" method="post">
-                <button type="submit" name="left"><</button>
+                <button type="submit" name="left" class="btn-left"><</button>
                 <?php if($_SESSION['goal_select'] == 0){ ?>
                     <span><?php echo '購入目標'; ?></span>
                 <?php } elseif($_SESSION['goal_select'] == 1){ ?>
@@ -155,7 +155,7 @@ echo '</script>';
                     <span><?php echo '行動目標'; ?></span>
                 <?php } ?>
 
-                <button type="submit" name="right">></button>
+                <button type="submit" name="right" class="btn-right">></button>
             </form>
         </div>
 
