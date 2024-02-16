6e52ef02a7e8da9f94f315398054b84696f479f9
nn <fki2266261@stu.o-hara.ac.jp>
2024-02-16 10:14:45 +0900

    るび

--- src/app/accounts/family_add.php
+++ src/app/accounts/family_add.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </div>
 
                         <div class="control">
-                            <label for="gender_id"><ruby>性別<rt>せいかく</rt></ruby></label>
+                            <label for="gender_id"><ruby>性別<rt>せいべつ</rt></ruby></label>
                             <select name="gender_id[]">
                                 <option value="1" <?php echo (isset($savedData['gender_id'][$i]) && $savedData['gender_id'][$i] == 1) ? 'selected' : ''; ?>>女性</option>
                                 <option value="2" <?php echo (isset($savedData['gender_id'][$i]) && $savedData['gender_id'][$i] == 2) ? 'selected' : ''; ?>>男性</option>
