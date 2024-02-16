0f4252a3077590325964873107d372daa3fbb211
kawamura <ayabomax2003@gmail.com>
2024-02-16 10:37:16 +0900

    銀行の拒否、バリデーション

--- lib/conset_class.php
+++ lib/conset_class.php
@@ -9,7 +9,7 @@ class consent {
         $this->db = $db;
         $this->error = []; // 初期化
 
-        if (isset($_POST["consent_help_N"])){
+        if (isset($_POST["consent_help_Y"])){
             $help_id = $_POST["consent_help_id"];
 
             $stmt = $this->db->prepare("UPDATE help_log SET receive_flag = 1,consent_flag = 0 WHERE help_id = :help_id and consent_flag = 1");
@@ -19,7 +19,7 @@ class consent {
 
             echo "承認しました";
             
-        }elseif (isset($_POST["consent_mission_N"])){
+        }elseif (isset($_POST["consent_mission_Y"])){
             $mission_id = $_POST["consent_mission_id"];
 
             $stmt = $this->db->prepare("UPDATE mission_log SET receive_flag = 1,consent_flag = 0 WHERE mission_id = :mission_id and consent_flag = 1");
@@ -73,6 +73,8 @@ class consent {
 
 
             echo "承認しました";
+
+        //銀行拒否
         } elseif (isset($_POST["consent_debt_N"])){
             $debt_id = $_POST["consent_debt_id"];
             $stmt = $this->db->prepare("DELETE FROM debt WHERE debt_id = :debt_id");
