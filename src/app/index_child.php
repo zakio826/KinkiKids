2787dc8ae81b0b44b077701a870840d77ffa9f61
dance-hall <sofcondance@gmail.com>
2024-02-16 15:06:26 +0900

    返信ボタンいじり

--- src/app/index_child.php
+++ src/app/index_child.php
@@ -352,7 +352,7 @@ $index_child_class->message($db);
                     <?php $index_child_class->getFamilyUser(); ?>
                 </select>
                 <input type="text" name="message" required>
-                <button type="submit" class="btn">返信</button>
+                <button type="submit" class="btn_hensin">返信</button>
             </form>
                 
             </div>
